1668069eaaaa8381f4716cebfc0b864a458185f5
Noxvill <gmo_mmo@live.cl>
2023-12-10 20:05:29 -0300

    cambios en index y html

---
+++ assets/js/propiedades.js
@@ -0,0 +1,25 @@
+const propiedades_alquiler = [
+    {
+    nombre: '',
+    src: '',
+    descripcion: '',
+    ubicacion: '',
+    habitaciones: 0,
+    costo: 0,
+    smoke: false,
+    pets: true
+    }
+    ]
+
+    const propiedades_venta = [
+        {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: 0,
+        costo: 0,
+        smoke: false,
+        pets: true
+        }
+        ]
