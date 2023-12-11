ee4eb769703af1f32dcf853d99f59a6f4bd3ff50
Noxvill <gmo_mmo@live.cl>
2023-12-10 23:45:20 -0300

    cambios

---
+++ assets/js/propiedades_alquiler.js
@@ -0,0 +1,50 @@
+// arreglo con propiedades en alquiler
+
+const propiedades_alquiler = [
+  {
+    nombre: "Apartamento en el centro de la ciudad",
+    src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=6",
+    descripcion: "Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    baños: 2,
+    costo: 2.000,
+    smoke: false,
+    pets: true,
+  },
+
+  {
+    nombre: "Apartamento luminoso con vista al mar",
+    src:"https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80",
+    descripcion: "Este hermoso apartamento ofrece una vista impresionante al mar",
+    ubicacion: "456 Ocean Avenue, Seaside Town, CA 56789",
+    habitaciones: 3,
+    baños: 3,
+    costo: 2.500,
+    smoke: true,
+    pets: true,
+  },
+
+  {
+    nombre: "Condominio moderno en zona residencial",
+    src:"https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+    descripcion: "Este elegante condominio moderno está ubicado en una tranquila zona residencial",
+    ubicacion: "123 Main Street, Anytown, CA 91234",
+    habitaciones: 2,
+    baños: 2,
+    costo: 2.200,
+    smoke: false,
+    pets: false,
+  },
+  {
+    nombre: "Casona patronal en la séptima región",
+    src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.prohabiter.cl%2Fpropiedad%2Fgran-casona-patronal-en-pleno-corazon-de-romeral&psig=AOvVaw145JFZQVsxnjuXrJNfAcuh&ust=1702338139448000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCLCyyfWFhoMDFQAAAAAdAAAAABAD",
+    descripcion: "Amplia casona patronal ubicada cerca de los valles de la séptima región",
+    ubicacion: "Cerro Fátima, km 24, Talca",
+    habitaciones: 6,
+    baños: 4,
+    costo: 8.000,
+    smoke: true,
+    pets: true,
+  },
+];
---
+++ assets/js/propiedades_venta.js
@@ -0,0 +1,91 @@
+// arreglo con propiedades en venta
+
+const propiedades_venta = [
+  {
+    nombre: "Apartamento de lujo en zona exclusiva",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    descripcion: "Apartamento de lujo en zona exclusiva",
+    ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
+    habitaciones: 4,
+    baños: 4,
+    costo: 5000,
+    smoke: false,
+    pets: false,
+  },
+
+  {
+    nombre: "Apartamento acogedor en la montaña",
+    src: "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+    descripcion: "Este apartamento acogedor está situado en lo alto de una montaña con impresionantes vistas",
+    ubicacion: "789 Mountain Road, Summit Peaks, CA 23456",
+    habitaciones: 2,
+    baños: 1,
+    costo: 1200,
+    smoke: true,
+    pets: true,
+  },
+
+  {
+    nombre: "Penthouse de lujo con terraza panorámica",
+    src: "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+    descripcion: "Este penthouse de lujo ofrece una terraza panorámica con vistas espectaculares",
+    ubicacion: "567 Skyline Avenue, Skyview City, CA 56789",
+    habitaciones: 3,
+    baños:3,
+    costo: 4500,
+    smoke: false,
+    pets: true,
+  },
+
+  {
+    nombre: "Cabaña acogedora en el bosque",
+    src: "https://huilohuilo.com/content/uploads/2019/06/Cabañas-del-bosque-e1559768027436.jpg",
+    descripcion: "Cómoda y acogedora cabaña en medio del bosque, cuenta con comodidades básicas para disfrutar de un hermoso tiempo de desconexión",
+    ubicacion: "Sector Santa Lucía Alto, km 33, Yungay, Ñuble",
+    habitaciones: 2,
+    baños: 2,
+    costo: 3000,
+    smoke: true,
+    pets: true,
+  }
+];
+
+// Función para renderizar las propiedades
+
+const rendersellproperties = () =>{
+
+  const divVenta = document.getElementById("venta")
+
+  propiedades_venta.forEach((propiedades_venta) =>{
+
+divVenta.innerHTML += 
+
+  
+  "<div class='row'>"+
+  "<div class='col-md-4 mb-4'>"+
+  "<div class='card'>"+
+  "<img src="+propiedades_venta.src+" class='card-img-top' alt='Imagen del departamento'/>"+
+  "<div class='card-body'>"+
+ "<h5 class='card-title'>"
+ +propiedades_venta.nombre+
+ "</h5>"
+ + "<p class='card-text'>"
+ +propiedades_venta.descripcion+
+"</p>"+"<i class='fas fa-map-marker-alt'>"+"</i>"+"<p>"+propiedades_venta.ubicacion+"</p>"+
+"<p>"+"<i class='fas fa-bed'>"+"</i>"+" "+propiedades_venta.habitaciones+ " "+ "habitaciones"+" "+"|"+" "+"<i class='fas fa-bath'>"+"</i>" +" "+propiedades_venta.baños+ " "+ "baños"+ "</p>"+
+
+"<p>"+"<i class='fas fa-dollar-sign'>"+"</i>"+propiedades_venta.costo+
+"</p>"+
+
+"<p class='text-danger'>"+"<i class='fas fa-smoking-ban'>"+"</i>"+" "+ "No se permite fumar"+ "</p>"+
+"<p class='text-danger'>"+"<i class='fa-solid fa-ban'>"+"</i>"+" "+propiedades_venta.pets+"</p>"
+
+"</div>"+
+"</div>"
+
+
+})
+
+}
+
+rendersellproperties()
